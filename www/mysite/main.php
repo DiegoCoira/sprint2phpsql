cf5875324541b3e466f4f05026e5271030969f68
Diego <dcoirap@fpcoruna.afundacion.org>
2022-10-05 14:48:13 +0200

    Inicio main.php

--- www/mysite/main.php
+++ www/mysite/main.php
@@ -1,4 +1,29 @@
 <?php
 	$db = mysqli_connect('local host', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión eestablecida</h1>
+
+<html>
+	<body>
+		<h1>Conexion establecida</h1>
+		<?php
+		// Lanzar una query
+			$query = 'SELECT * FROM tCanciones';
+			mysqli_query($db, $query) or die('Query error');
+			$query = 'SELECT * FROM tCanciones';
+			$result = mysqli_query($db, $query)  or die('Query error');
+			// Recorrer el resultado
+			while ($row = mysqli_fetch_array($result)) {
+				echo $row['titulo'];
+				echo '<br>';
+				echo $row[2];
+				echo '<br>';
+			}
+		mysqli_close($db);
+		?>
+
+		<ul>
+			
+		</ul>
+
+	</body>
+</html>
