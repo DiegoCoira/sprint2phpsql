34a404fd55fefd90c7b9e4cae839a5315c9b1872
Diego <dcoirap@fpcoruna.afundacion.org>
2022-10-06 11:59:58 +0200

    main.php no terminado

--- www/mysite/main.php
+++ www/mysite/main.php
@@ -7,23 +7,47 @@
 		<h1>Conexion establecida</h1>
 		<?php
 		// Lanzar una query
-			$query = 'SELECT * FROM tCanciones';
+			$query = 'SELECT * FROM tUsuarios';
 			mysqli_query($db, $query) or die('Query error');
-			$query = 'SELECT * FROM tCanciones';
-			$result = mysqli_query($db, $query)  or die('Query error');
-			// Recorrer el resultado
-			while ($row = mysqli_fetch_array($result)) {
-				echo $row['titulo'];
-				echo '<br>';
-				echo $row[2];
-				echo '<br>';
-			}
-		mysqli_close($db);
+			$query = 'SELECT * FROM tUsuarios';
+			$row = mysqli_query($db, $query)  or die('Query error');
 		?>
-
-		<ul>
-			
-		</ul>
-
+		<table>
+			<ul><h3>tUsuarios</h3>
+				<?php
+					echo '<li id= href="/detail.php?id='.$row[1].'</li>';
+					echo '<li>'.$row[2].'</li>';
+					echo '<li>'.$row[3].'</li>';
+					echo '<li>'.$row[4].'</li>';
+					echo '<li>'.$row[5].'</li>';
+				?>
+			</ul>
+				<?php
+					$query = 'SELECT * FROM tCanciones';
+					$row = mysql_query($db,$query) or die('Query error');
+				?>
+			<ul><h3>tCanciones</h3>
+				<?php
+				echo '<li>'.$row[1].'</li>';
+				echo '<li>'.$row[2].'</li>';
+				echo '<li src='.$row[3].'>''</li>';
+				echo '<li>'.$row[4].'</li>';
+				echo '<li>'.$row[5].'</li>';
+				echo '<li>'.$row[6].'</li>';
+				?>
+			</ul>
+				<?php
+					$query = 'SELECT * FROM tComentarios';
+					$row = mysql_query($db,$query) or die('Query error')
+				?>
+			<ul><h3>tComentarios</h3>
+				<?php
+				echo '<li>'.$row[1].'</li>';
+				echo '<li>'.$row[2].'</li>';
+				echo '<li>'.$row[3].'</li>';
+				echo '<li>'.$row[4].'</li>';
+				?>
+			</ul>
+		mysqli_close($db);
 	</body>
 </html>
