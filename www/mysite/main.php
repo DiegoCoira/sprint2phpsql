0955f8ba1be2307d8039dce96a090db1ddd3bb0e
Diego <dcoirap@fpcoruna.afundacion.org>
2022-10-11 13:32:49 +0200

    Añadido main y detail (no completos)

--- www/mysite/main.php
+++ www/mysite/main.php
@@ -5,49 +5,44 @@
 <html>
 	<body>
 		<h1>Conexion establecida</h1>
-		<?php
-		// Lanzar una query
-			$query = 'SELECT * FROM tUsuarios';
-			mysqli_query($db, $query) or die('Query error');
-			$query = 'SELECT * FROM tUsuarios';
-			$row = mysqli_query($db, $query)  or die('Query error');
-		?>
 		<table>
-			<ul><h3>tUsuarios</h3>
-				<?php
-					echo '<li id= href="/detail.php?id='.$row[1].'</li>';
+			<?php
+			// Lanzar una query
+			$select = 'SELECT * FROM tUsuarios';
+			$query = mysqli_query($db, $query)  or die('Query error');
+			while($row = mysqli_fetch_array($query)) {
+					<h3>tUsuarios</h3>
+					echo '<li>'.$row[1].'</li>';
 					echo '<li>'.$row[2].'</li>';
 					echo '<li>'.$row[3].'</li>';
 					echo '<li>'.$row[4].'</li>';
 					echo '<li>'.$row[5].'</li>';
-				?>
-			</ul>
-				<?php
-					$query = 'SELECT * FROM tCanciones';
-					$row = mysql_query($db,$query) or die('Query error');
-				?>
-			<ul><h3>tCanciones</h3>
-				<?php
-				echo '<li>'.$row[1].'</li>';
-				echo '<li>'.$row[2].'</li>';
-				echo '<li src='.$row[3].'>''</li>';
-				echo '<li>'.$row[4].'</li>';
-				echo '<li>'.$row[5].'</li>';
-				echo '<li>'.$row[6].'</li>';
-				?>
-			</ul>
-				<?php
-					$query = 'SELECT * FROM tComentarios';
-					$row = mysql_query($db,$query) or die('Query error')
-				?>
-			<ul><h3>tComentarios</h3>
-				<?php
-				echo '<li>'.$row[1].'</li>';
-				echo '<li>'.$row[2].'</li>';
-				echo '<li>'.$row[3].'</li>';
-				echo '<li>'.$row[4].'</li>';
-				?>
-			</ul>
+			}
+			?>
+			<?php
+				$select = 'SELECT * FROM tCanciones';
+				$query = mysqli_query($db,$query) or die('Query error')
+				while ($row = mysqli_fetch_array($query)) {
+					<ul>
+					echo '<li id='$row[1]'href=/detail.php?id='$row[1]'>'.$row[1].'</li>';						echo '<li>'.$row[2].'</li>';						echo '<li src='.$row[3]'>'.$row[3].'>''</li>';
+					echo '<li>'.$row[4].'</li>';
+					echo '<li>'.$row[5].'</li>';
+					echo '<li>'.$row[6].'</li>';
+					</ul>
+				}
+			<?php
+				$select = 'SELECT * FROM tComentarios';
+				$query = mysqli_query($db,$select) or die('Query error')
+				while($row = mysqli_fetch_array($query)){
+					<ul>
+					<h3>tComentarios</h3>
+					echo '<li>'.$row[1].'</li>';
+					echo '<li>'.$row[2].'</li>';
+					echo '<li>'.$row[3].'</li>';
+					echo '<li>'.$row[4].'</li>';
+					</ul>
+				}
+			?>
 		mysqli_close($db);
 	</body>
 </html>
